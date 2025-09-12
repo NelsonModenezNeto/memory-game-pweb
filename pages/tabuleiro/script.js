7f1c4071222adbcbbd392c7956f84e9405071edf
joaomansano-dev <j.victor.mansano@gmail.com>
2025-09-12 12:06:06 -0300

    feat: adicao de tabuleiro e componentes
    Adicionei o tabuleiro juntamente com uma função responsável pela criação do tabuleiro responsivo.
    
    Além disso, coloquei informações necessárias como num de jogadas, tempo de jogo e etc

---
+++ pages/tabuleiro/script.js
@@ -0,0 +1,19 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   const tabuleiro = document.querySelector(".tabuleiro");
+
+//   function createBoard(gridSize) {
+//     tabuleiro.style.setProperty("--grid-size", gridSize);
+
+//     tabuleiro.innerHTML = "";
+
+//     const totalCards = gridSize * gridSize;
+
+//     for (let i = 0; i < totalCards; i++) {
+//       const card = document.createElement("div");
+//       card.classList.add("cards");
+//       tabuleiro.appendChild(card);
+//     }
+//   }
+
+//   createBoard(4);
+// });
