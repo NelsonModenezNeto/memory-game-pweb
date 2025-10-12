242d60eda4566a5ae707a7506e20fe1ec3f78eb9
joaomansano-dev <j.victor.mansano@gmail.com>
2025-10-12 19:46:39 -0300

    feat: globalização do css
    Conforme pedidos do guilherme, fiz um css global que une estilizações que estavam repetidas durante nosso projeto

--- scripts/start-game.js
+++ scripts/start-game.js
@@ -2,9 +2,9 @@ import { Game } from "./game.js";
 
 const playButton = document.querySelector("#playButton");
 
-playButton.addEventListener('click', () => {
-    const game = new Game({
-        tableSize: 2
-    });
-    localStorage.setItem('game', JSON.stringify(game));
-})
+playButton.addEventListener("click", () => {
+  const game = new Game({
+    tableSize: 4,
+  });
+  localStorage.setItem("game", JSON.stringify(game));
+});
