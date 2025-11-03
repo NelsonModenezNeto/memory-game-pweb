4df5c14d8dbd558e933201d6c441c75629e4b52c
otbox <otboxmarques88@gmail.com>
2025-11-02 23:38:52 -0300

    Entrega 2

--- pages/tabuleiro/script.js
+++ pages/tabuleiro/script.js
@@ -1,142 +1,10 @@
+import { cardDictionary } from "./cardsData.js";
 // ============================================================================
 // SEÇÃO 1: CONFIGURAÇÕES E CONSTANTES
 // ============================================================================
 
+
 // Dicionário
-const cardDictionary = {
-  mario: {
-    image: "assets/card_images/bald_mario.png",
-    sound: "super-mario-death-sound-sound-effect.mp3",
-  },
-  geralt: {
-    image: "assets/card_images/geralt_meme.png",
-    sound: "geralt_soitbegins.mp3",
-  },
-  plankton: {
-    image: "assets/card_images/plankton_meme.png",
-    sound: "plankton-oooooh.mp3",
-  },
-  goku: {
-    image: "assets/card_images/serious_goku_meme.png",
-    sound: "oi-eu-sou-goku.mp3",
-  },
-  sonic: {
-    image: "assets/card_images/shocked_sonic.png",
-    sound: "sega-hd.mp3",
-  },
-  gru: {
-    image: "assets/card_images/gru-henning.jpeg",
-    sound: "voce-e-careca.mp3",
-  },
-  shrek: {
-    image: "assets/card_images/shrek.jpg",
-    sound: "faz-o-urro_3BwvHjc.mp3",
-  },
-  stonks: {
-    image: "assets/card_images/stonks.png",
-    sound: "different-variations-for-stonks-sound-effect.mp3",
-  },
-  heisenberg: {
-    image: "assets/card_images/breaking_bad.png",
-    sound: "say-my-name-heisenbarg.mp3",
-  },
-  pix: {
-    image: "assets/card_images/pix.png",
-    sound: "e-o-pix-nada-ainda.mp3",
-  },
-  werewolf: {
-    image: "assets/card_images/werewolf.png",
-    sound: "werewolf-howling-sfx.mp3",
-  },
-  nice: {
-    image: "assets/card_images/nice.png",
-    sound: "-click-nice.mp3",
-  },
-  kratos: {
-    image: "assets/card_images/kratos.png",
-    sound: "ares_8E1uEzn.mp3",
-  },
-  win_xp: {
-    image: "assets/card_images/win_xp.png",
-    sound: "erro.mp3",
-  },
-  tech: {
-    image: "assets/card_images/tech.png",
-    sound: "123444.mp3",
-  },
-  wow: {
-    image: "assets/card_images/wow.png",
-    sound: "greenscreen-wow.mp3",
-  },
-  let_him_cook: {
-    image: "assets/card_images/let_him_cook.png",
-    sound: "let-him-cook_vwv2ZJq.mp3",
-  },
-  vegeta: {
-    image: "assets/card_images/vegeta.png",
-    sound: "8000.mp3",
-  },
-  spider_pizza: {
-    image: "assets/card_images/spider_pizza.png",
-    sound: "mp3_e9ca8310-8d3f-11e9-9016-8b78caf46556.mp3",
-  },
-  bald_batman: {
-    image: "assets/card_images/bald_batman.png",
-    sound: "im-batman.mp3",
-  },
-  homer: {
-    image: "assets/card_images/homer.png",
-    sound: "doh_r4RZcVw.mp3",
-  },
-  papaco: {
-    image: "assets/card_images/papaco.png",
-    sound: "ate-um-outro-dia_CyY3XQi.mp3",
-  },
-  pelé: {
-    image: "assets/card_images/pelé.png",
-    sound: "meu-filme_50.mp3",
-  },
-  kowalski: {
-    image: "assets/card_images/kowalski.png",
-    sound: "meme-recruta-relatorio.mp3",
-  },
-  apodreca: {
-    image: "assets/card_images/apodreca.png",
-    sound: "greenscreen-wow.mp3",
-  },
-  baguio_doido: {
-    image: "assets/card_images/baguio_doido.png",
-    sound: "greenscreen-wow.mp3",
-  },
-  calabreso: {
-    image: "assets/card_images/calabreso.png",
-    sound: "greenscreen-wow.mp3",
-  },
-  cj: {
-    image: "assets/card_images/cj.png",
-    sound: "greenscreen-wow.mp3",
-  },
-  confia: {
-    image: "assets/card_images/confia.png",
-    sound: "greenscreen-wow.mp3",
-  },
-  genius: {
-    image: "assets/card_images/genius.jpg",
-    sound: "greenscreen-wow.mp3",
-  },
-  la_paloma: {
-    image: "assets/card_images/la_paloma.png",
-    sound: "greenscreen-wow.mp3",
-  },
-  mao_cena: {
-    image: "assets/card_images/mao_cena.png",
-    sound: "greenscreen-wow.mp3",
-  },
-  nazare: {
-    image: "assets/card_images/nazare.png",
-    sound: "greenscreen-wow.mp3",
-  },
-};
 
 // Configuração de tempo
 const timeByTableSize = {
@@ -151,8 +19,9 @@ const tableWidth = {
   2: 230,
   4: 420,
   6: 620,
-  8: 800,
+  8: 640,
 };
+
 
 // URL base para os sons
 const pathSound = "https://www.myinstants.com/media/sounds/";
@@ -166,6 +35,7 @@ const btnJoker = document.querySelector("#joker-all-cards");
 const btnJokerBackGame = document.querySelector("#joker-back-game");
 const table = document.querySelector(".tabuleiro");
 const game = JSON.parse(localStorage.getItem("game"));
+const cardSize = game.tableSize == 8 ? "60px" : "80px"; 
 
 // Variáveis de controle do jogo
 let flippedCards = [];
@@ -334,8 +204,8 @@ function resetGame() {
     const cardKey = cardKeys[i];
 
     card.classList.add("cards");
-    card.style.width = "80px";
-    card.style.height = "80px";
+    card.style.width = cardSize;
+    card.style.height = cardSize;
 
     card.style.backgroundImage = `url('../../${cardDictionary[cardKey].image}')`;
     card.dataset.cardKey = cardKey;
@@ -408,7 +278,7 @@ function markAsMatched(card1, card2) {
     // Obtém o som usando a chave do dicionário
     const cardKey = card1.dataset.cardKey;
     const soundFile = cardDictionary[cardKey].sound;
-    const soundPath = pathSound + soundFile;
+    const soundPath = pathSound + soundFile;12
 
     const sound = new Audio(soundPath);
     sound.play().catch((err) => console.log("Erro ao tocar som:", err));
@@ -517,8 +387,11 @@ for (let i = 0; i < game.tableSize * game.tableSize; i++) {
   const cardKey = cardKeys[i];
 
   card.classList.add("cards");
-  card.style.width = "80px";
-  card.style.height = "80px";
+  console.log(cardSize)
+  card.style.width = cardSize;
+  card.style.height = cardSize;
+  
+
 
   // Define a imagem usando o dicionário
   card.style.backgroundImage = `url('../../${cardDictionary[cardKey].image}')`;
